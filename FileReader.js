ba95daf73230023b77758e12979b2ab16872a4c9
黐麟 <tirin.script.modder@gmail.com>
2017-09-04 02:55:01 +0900

    Update FileReader.js

--- FileReader.js
+++ FileReader.js
@@ -1,2 +1,8 @@
 function read(path){
+ var reader=new BufferedReader(new InputStreamReader(new FileInputStream(new File(path)),'UTF-8'));
+ var resource='';
+ for(var source=reader.readLine();source!=null;source=reader.readLine())
+  resource+=source+'\n';
+ return resource.slice(0,-1);
+ reader.close();
 }
