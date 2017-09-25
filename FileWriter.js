d1124d70799c10ea27c1c505a0daacb03310cfc7
黐麟 <tirin.script.modder@gmail.com>
2017-09-25 12:15:42 +0900

    Update and rename FileWriter.js to write.js

--- FileWriter.js
+++
@@ -1,5 +0,0 @@
-function write(path,data){
- var writer=new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(path),false),'UTF-8'));
- writer.write(data);
- writer.close();
-}
---
+++ write.js
@@ -0,0 +1,13 @@
+function write(path,data){
+ var writer=new BufferedWriter(
+  new OutputStreamWriter(
+   new FileOutputStream(
+    new File(path),
+    false
+   ),
+   'UTF-8'
+  )
+ );
+ writer.write(data);
+ writer.close();
+}
